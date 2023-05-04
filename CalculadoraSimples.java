e896d1c685174958cfaa231db547fb235527ff4d
Henriquinhocraycray <130668229+Henriquinhocraycray@users.noreply.github.com>
2023-05-04 08:45:33 -0300

    commit do dia 04 de Maio

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
         int n1, n2, total, op;
 
         //Criação e instância do objeto de entrada
-        Scanner entrada = new Scanner(System.in);
+            Scanner entrada = new Scanner(System.in);
 
         do{
 
@@ -23,6 +23,19 @@ public static void main(String[] args) {
 
             if(op == 1){
                 System.out.println("n/t/t/t/ -- Soma -- \n");
+
+        //Entrada
+            System.out.println("Informe N1: ");
+            n1 = entrada.nextInt();
+            System.out.println("Informe N2: ");
+            n2 = entrada.nextInt();
+
+        //Processamento
+            total = n1 + n2;
+
+        //Saída
+            System.out.printf("%d + %d = %d", n1, n2, total);
+
             } else if(op == 2) {
                 System.out.println("\n\t\t\t -- Subtração -- \n");
             }else if(op == 3) {
